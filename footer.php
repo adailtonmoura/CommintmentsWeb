4022117737321a0663acd4b4223034b74930d7d0
adailtonmoura <aluno@ifslab.edu.br>
2020-02-04 08:40:23 -0300

    Importei O jQuery

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
-
-<footer class="rodape"style="background-color:black;">
-
-
-</footer>
-
-</body>
+        <footer class="rodape bg-warning">
+            <script src="vendor/components/jquery/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.3/js/tether.min.js"></script>
+            <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+        </footer>
+    </body>
+    
 </html>
--- header.php
+++ header.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt_BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
-        <title>Commitments</title>
-</head>
-<body>
-        <header class="cabecalho">
-       
-<nav id="menu"class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        
-        <li class="nav-item active">
-          <a class="navbar-brand" href="#">
-    <img src="image/post.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    Commitments
-  </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-</nav>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="css/style.css">
+		<link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+		<title>Commitments</title>
+	</head>
+	<body>
+        <header class="cabecalho">  
+			<nav id="menu"class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="navbar-brand" href="#" onclick="carregar('view/home.php')">
+								<img src="image/post.png" width="30" height="30" class="d-inline-block align-top" alt=""> Commitments
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Login</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Registro</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
         </header>
